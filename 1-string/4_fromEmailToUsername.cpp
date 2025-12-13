30a4111fcf99ae4e1646fb7101ab925745b8fb4f
Spada-Michael13 <michaelspada104@gmail.com>
2025-12-13 11:53:50 +0100

    Update 4_fromEmailToUsername.cpp
    es 4 fatto,non rispettati i consigli

--- 1-string/4_fromEmailToUsername.cpp
+++ 1-string/4_fromEmailToUsername.cpp
@@ -22,6 +22,11 @@ string emails[N] = {
     "marta.leone@studio.alberghetti.it"
     };
 
+    string minuscolo(string s){
+    for(int i = 0; i < s.length(); i++)
+        s.at(i) = tolower(s.at(i));
+    return s;
+}
 
 /*
     TODO: la seguente funzione prende in input una string s minuscola e modifica il primo carattere rendendolo maiuscolo.
@@ -30,8 +35,15 @@ string emails[N] = {
     Hint: usare la funzione toupper(), vedi funzione minuscolo() in esercizio 3_userGenerator.cpp
 
 */
-string primoCarattereMaiuscolo(string &s) {
+string primoCarattereMaiuscolo(int i) {
+for(int y=0;y<N;y++)
+{
+    int x=emails[y].find(".");
+    emails[y].at(0)=toupper(emails[y].at(0));
+     emails[y].at(x+1)=toupper(emails[y].at(x+1));
 
+}
+return emails[i];
 }
 
 int main() {
@@ -46,19 +58,22 @@ int main() {
         string email = emails[i]; //email corrente
 
         //TODO: rimuovere la parte dopo la chiocciola
-
+int x=email.find("@");
+int z=email.size();
+emails[i].erase(x,z);
         // TODO: spezzare la stringa in due parti diverse: nome e cognome (utilizzare il punto come criterio per separare)
 
         // TODO: rendere maiuscolo il primo carattere di nome e cognome (usare la funzione primoCarattereMaiuscolo)
 
         // TODO: inserire in nomiCognomi[i] l'insieme delle due stringhe (concatenarle con l'operatore +)
-        
+
     }
 
     // stampa il risultato
     cout << "Risultato:\n";
     for (int i = 0; i < N; i++) {
-        cout << nomiCognomi[i] << endl;
+            emails[i]=primoCarattereMaiuscolo(i);
+        cout << emails[i] << endl;
     }
 
     return 0;
