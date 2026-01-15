de906a72d965c08a13fa037d8205b921dd159656
Spada-Michael13 <michaelspada104@gmail.com>
2026-01-15 08:11:00 +0100

    Update 1_studente_basic.cpp

--- 3-make/1_studente_basic.cpp
+++ 3-make/1_studente_basic.cpp
@@ -22,8 +22,20 @@ int main()
     /*********************** Creazione di oggetti ******************************/
     // TODO Creare un nuovo studente "Mario" con eta 17 e media 7
     // TODO Creare un nuovo studente "Maria"  (questa volta con un puntatore) con eta 18 e media 8
+Studente s;
 
+s.nome="Mario";
+s.eta=17;
+s.media=7;
 
+Studente s2;
+Studente *p=&s2;
+p->nome="Maria";
+p->eta=18;
+p->media=8;
+
+s.stampaStudente();
+p->stampaStudente();
     /************************** Visualizzazione ********************************/
     // TODO Visualizzare il nome e la media sia di Mario che Maria
 
